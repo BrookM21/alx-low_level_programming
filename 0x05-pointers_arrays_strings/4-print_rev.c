98fc9878745fea649445e01c5cda72ddc545a3b8
Biruk Merage <biruk.m@psseth.com>
2021-09-07 15:44:22 +0300

    REVERSAL

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * print_rev -  prints a string, in reverse, followed by a new line.
  * @s: the pointer;
  * Return: Alwaus 0.
  */
-
 void print_rev(char *s)
 {
 	int h = -1;
